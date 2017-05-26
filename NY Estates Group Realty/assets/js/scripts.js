854d6ba045026424d59d50a2abf9610d078cd5f6
grossj0816 <juwangross0815@gmail.com>
2017-05-26 19:04:19 -0400

    Changed team cards for TeamPage.html, and added a link to home page in sidenav for mobile.

--- NY Estates Group Realty/assets/js/scripts.js
+++ NY Estates Group Realty/assets/js/scripts.js
@@ -1,5 +1,7 @@
+//enabling parallax containers to show images
 $(document).ready(function(){
   $('.parallax').parallax(); });
 
+//enables sidenav for mobile responsivenesss
 $(document).ready(function(){
   $(".button-collapse").sideNav(); });
