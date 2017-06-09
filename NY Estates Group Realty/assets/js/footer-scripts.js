0c2e2a91d6c99c78a19f9574a01d1fcab6dde553
grossj0816 <juwangross0815@gmail.com>
2017-06-08 20:36:28 -0400

    Revised a script that updated the Dale Davis employee bio page.

--- NY Estates Group Realty/assets/js/footer-scripts.js
+++ NY Estates Group Realty/assets/js/footer-scripts.js
@@ -14,5 +14,5 @@ function updateFooter(footerParagraph, footerOffice, footerAddress, footerNumber
   elFooterNumber.textContent = footerNumber;
 }
 
-updateFooter('NY Estates Group Realty was funded 14 years ago by Andrea S Gross. From starting NY Estates G.R. on her own, Andrea has built this company to provide the best service for people who want to purchase commercial properties and homes that they will call theirs forever.',
+updateFooter('NY Estates Group Realty was funded 14 years ago by Andrea S Gross. From starting NY Estates G.R. on her own,  Andrea has built this company to provide the best service for people who want to purchase commercial properties and homes that they will call theirs forever.',
 'Saint Albans Office', '205754 Linden Blvd, Saint Albans, NY 11412', '(718) 341-2618');
---
+++ NY Estates Group Realty/assets/js/member2pg-scripts.js
@@ -0,0 +1,10 @@
+/*This scripts is designed to acces and modify the employee bio.*/
+
+
+function updateMember2Bio(memberBio)
+{
+  var elMemBio = document.getElementById('member2Info');
+  elMemBio.textContent = memberBio;
+}
+
+updateMember2Bio("Dale Davids loves to provide excellent full service real estate in the area of sales, rental and management of residential and commercial properties in the State of New York. Dale has been working with NY Estates Group Realty for about two years now, but he has twelve years of experience and a strong neural network of Sellers, Buyers, and Investors. Dale offers professional services such as Sales, Rental, Management, Legal Process Server, Notary Public Commercial Insurance, and Credit Restoration. With years of education from 2003 obtaining his Business Administration Degree to becoming a Certified Paralegal in 2015, but also with the experience Dale has gained using all that he has learned, he is able to make the Buyer's dreams in buying the perfect home or establishment come true.");
