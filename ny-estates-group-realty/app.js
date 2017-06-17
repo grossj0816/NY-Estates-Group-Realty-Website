59e74707ddc3166eacfddc054a56127721a1094a
grossj0816 <juwangross0815@gmail.com>
2017-06-16 22:54:23 -0400

    Edited app.js file to run all the webpages.

--- ny-estates-group-realty/app.js
+++ ny-estates-group-realty/app.js
@@ -1,11 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
 
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+
+app.get('/IndexPage.html', function(req, res){
+  res.sendFile(path.join(__dirname+'IndexPage.html'));
 });
 
+app.get('/MissionPage.html', function(req, res){
+  res.sendFile(path.join(__dirname+'MissionPage.html'));
+});
+
+app.get('/ServicesPage.html', function(req, res){
+  res.sendFile(path.join(__dirname+'ServicesPage.html'));
+});
+
+app.get('/TeamPage.html', function(req, res){
+  res.sendFile(path.join(__dirname+'TeamPage.html'));
+});
+
+app.get('/TeamMemberPage1.html', function(req, res){
+  res.sendFile(path.join(__dirname+'TeamMemberPage1.html'));
+});
+
+app.get('/TeamMemberPage2.html', function(req, res){
+  res.sendFile(path.join(__dirname+'TeamMemberPage2.html'));
+});
+
+app.get('/TeamMemberPage3.html', function(req, res){
+  res.sendFile(path.join(__dirname+'TeamMemberPage3.html'));
+});
+
+app.get('/TeamMemberPage4.html', function(req, res){
+  res.sendFile(path.join(__dirname+'TeamMemberPage4.html'));
+});
 
 app.listen(3000, function(){
   console.log('Server started on port 3000...');
